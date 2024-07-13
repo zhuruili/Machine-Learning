2de38c2a7484d9cfe211499b209adf05266c2bfc
LiZhuRui <157603873+zhuruili@users.noreply.github.com>
2024-07-13 21:25:50 +0800

    优化了代码结构，新增了对线性回归结果的展示

--- LR_simple.py
+++ LR_simple.py
@@ -1,15 +1,20 @@
+# env: Python 3.9.18
+# 简单线性回归示例
 import torch
-
 import torch.nn as nn
 import torch.optim as optim
 
-# Hyper-parameters
+import matplotlib.pyplot as plt
+
+# 超参数设置
 input_size = 1
 output_size = 1
 num_epochs = 500
 learning_rate = 0.001
+save_model = False # 是否保存模型
+SAVEPATH = 'Rookie\part_2_items\MachineLearning\PyTorch\model\LR_simple.ckpt' # 模型保存路径
 
-# Toy dataset
+# 数据集
 x_train = torch.tensor([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168], 
                         [9.779], [6.182], [7.59], [2.167], [7.042], 
                         [10.791], [5.313], [7.997], [3.1]], dtype=torch.float32)
@@ -20,19 +25,21 @@ y_train = torch.tensor([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
 
 # Check if GPU is available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
 
 # Linear regression model
 model = nn.Linear(input_size, output_size).to(device)
 
 # Loss and optimizer
 criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)  
 
 # Move tensors to the configured device
 x_train = x_train.to(device)
 y_train = y_train.to(device)
 
 # Train the model
+print('--- Start Training ---')
 for epoch in range(num_epochs):
     # Forward pass
     outputs = model(x_train)
@@ -45,9 +52,23 @@ for epoch in range(num_epochs):
     
     if (epoch+1) % 50 == 0:
         print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+print('--- End Training ---')
+
+# Plot the graph
+predicted = model(x_train).detach().cpu().numpy()
+plt.plot(x_train.cpu().numpy(), y_train.cpu().numpy(), 'ro', label='Original data')
+plt.plot(x_train.cpu().numpy(), predicted, label='Fitted line')
+plt.legend()
+plt.show()
 
 # Save the model checkpoint
-torch.save(model.state_dict(), 'Rookie\part_2_items\MachineLearning\PyTorch\model\LR_simple.ckpt')
+if save_model:
+    torch.save(model.state_dict(), SAVEPATH)
+    print(f'Model saved to {SAVEPATH}')
+else:
+    print('save_model: False')
+
+
 
 """
 这段Python代码使用了PyTorch库来进行简单的线性回归训练。
@@ -62,7 +83,7 @@ torch.optim：优化算法库，用于训练模型。
 input_size 和 output_size：分别定义了输入和输出的特征数量，这里都是1，因为我们的数据只有一个特征。
 num_epochs：训练的轮数，也就是模型会遍历多少次整个数据集。
 learning_rate：学习率，决定了模型在训练过程中参数更新的步长。
-然后，你创建了一个玩具数据集：
+然后，你创建了一个数据集：
 
 x_train：输入数据，是一个15x1的张量（可以理解为矩阵），包含15个样本，每个样本有一个特征。
 y_train：对应的目标值，也是一个15x1的张量，包含了每个样本对应的真实输出。
